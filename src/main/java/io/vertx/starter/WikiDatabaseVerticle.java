22c822ebcb1b9adcd49e25320a718ccb259a8e38
Anand Raman <anaraman@publicisgroupe.net>
2018-05-14 14:25:00 +0100

    refactored to individual verticles

---
+++ src/main/java/io/vertx/starter/WikiDatabaseVerticle.java
@@ -0,0 +1,218 @@
+package io.vertx.starter;
+
+import static io.vertx.starter.WikiDatabaseVerticle.SqlQuery.CREATE_PAGES_TABLE;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.ResultSet;
+import io.vertx.ext.sql.SQLConnection;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikiDatabaseVerticle extends AbstractVerticle {
+
+  public static final String CONFIG_WIKIDB_JDBC_URL = "wikidb.jdbc.url";
+  public static final String CONFIG_WIKIDB_JDBC_DRIVER_CLASS = "wikidb.jdbc.driver_class";
+  public static final String CONFIG_WIKIDB_JDBC_MAX_POOL_SIZE = "wikidb.jdbc.max_pool_size";
+  public static final String CONFIG_WIKIDB_SQL_QUERIES_RESOURCE_FILE = "wikidb.sqlqueries.resource.file";
+
+  public static final String CONFIG_WIKIDB_QUEUE = "wikidb.queue";
+  private final Map<SqlQuery, String> sqlQueries = new HashMap<>();
+  private static final Logger LOGGER = LoggerFactory.getLogger(WikiDatabaseVerticle.class);
+  private JDBCClient dbClient;
+
+  public enum SqlQuery {
+    CREATE_PAGES_TABLE,
+    ALL_PAGES,
+    GET_PAGE,
+    CREATE_PAGE,
+    SAVE_PAGE,
+    DELETE_PAGE
+  }
+
+  public enum ErrorCodes {
+    NO_ACTION_SPECIFIED,
+    BAD_ACTION,
+    DB_ERROR
+  }
+
+
+  private void loadSqlQueries() throws IOException {
+
+    String queriesFile = config().getString(CONFIG_WIKIDB_SQL_QUERIES_RESOURCE_FILE);
+    InputStream queriesInputStream;
+    if (queriesFile != null) {
+      queriesInputStream = new FileInputStream(queriesFile);
+    } else {
+      queriesInputStream = getClass().getResourceAsStream("/db-queries.properties");
+    }
+
+    Properties queriesProps = new Properties();
+    queriesProps.load(queriesInputStream);
+    queriesInputStream.close();
+
+    sqlQueries.put(CREATE_PAGES_TABLE, queriesProps.getProperty("create-pages-table"));
+    sqlQueries.put(SqlQuery.ALL_PAGES, queriesProps.getProperty("all-pages"));
+    sqlQueries.put(SqlQuery.GET_PAGE, queriesProps.getProperty("get-page"));
+    sqlQueries.put(SqlQuery.CREATE_PAGE, queriesProps.getProperty("create-page"));
+    sqlQueries.put(SqlQuery.SAVE_PAGE, queriesProps.getProperty("save-page"));
+    sqlQueries.put(SqlQuery.DELETE_PAGE, queriesProps.getProperty("delete-page"));
+  }
+
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+
+    this.loadSqlQueries();
+
+    dbClient = JDBCClient.createShared(vertx, new JsonObject()
+        .put("url", "jdbc:hsqldb:file:db/wiki")
+        .put("driver_class", "org.hsqldb.jdbcDriver")
+        .put("max_pool_size", 30));
+
+    dbClient.getConnection(ar -> {
+      if (ar.failed()) {
+        startFuture.fail(ar.cause());
+      } else {
+        SQLConnection connection = ar.result();
+        connection.execute(sqlQueries.get(CREATE_PAGES_TABLE), createResult -> {
+          connection.close();
+          if (createResult.failed()) {
+            startFuture.fail(createResult.cause());
+          } else {
+            startFuture.complete();
+          }
+        });
+      }
+    });
+  }
+
+  public void onMessage(Message<JsonObject> message) {
+
+    if (!message.headers().contains("action")) {
+      LOGGER.error("No action header specified for message with headers {} and body {}",
+          message.headers(), message.body().encodePrettily());
+      message.fail(ErrorCodes.NO_ACTION_SPECIFIED.ordinal(), "No action header specified");
+      return;
+    }
+    String action = message.headers().get("action");
+
+    switch (action) {
+      case "all-pages":
+        fetchAllPages(message);
+        break;
+      case "get-page":
+        fetchPage(message);
+        break;
+      case "create-page":
+        createPage(message);
+        break;
+      case "save-page":
+        savePage(message);
+        break;
+      case "delete-page":
+        deletePage(message);
+        break;
+      default:
+        message.fail(ErrorCodes.BAD_ACTION.ordinal(), "Bad action: " + action);
+    }
+  }
+
+  private void fetchAllPages(Message<JsonObject> message) {
+    dbClient.query(sqlQueries.get(SqlQuery.ALL_PAGES), res -> {
+      if (res.succeeded()) {
+        List<String> pages = res.result()
+            .getResults()
+            .stream()
+            .map(json -> json.getString(0))
+            .sorted()
+            .collect(Collectors.toList());
+        message.reply(new JsonObject().put("pages", new JsonArray(pages)));
+      } else {
+        reportQueryError(message, res.cause());
+      }
+    });
+  }
+
+  private void fetchPage(Message<JsonObject> message) {
+    String requestedPage = message.body().getString("page");
+    JsonArray params = new JsonArray().add(requestedPage);
+
+    dbClient.queryWithParams(sqlQueries.get(SqlQuery.GET_PAGE), params, fetch -> {
+      if (fetch.succeeded()) {
+        JsonObject response = new JsonObject();
+        ResultSet resultSet = fetch.result();
+        if (resultSet.getNumRows() == 0) {
+          response.put("found", false);
+        } else {
+          response.put("found", true);
+          JsonArray row = resultSet.getResults().get(0);
+          response.put("id", row.getInteger(0));
+          response.put("rawContent", row.getString(1));
+        }
+        message.reply(response);
+      } else {
+        reportQueryError(message, fetch.cause());
+      }
+    });
+  }
+
+  private void createPage(Message<JsonObject> message) {
+    JsonObject request = message.body();
+    JsonArray data = new JsonArray()
+        .add(request.getString("title"))
+        .add(request.getString("markdown"));
+
+    dbClient.updateWithParams(sqlQueries.get(SqlQuery.CREATE_PAGE), data, res -> {
+      if (res.succeeded()) {
+        message.reply("ok");
+      } else {
+        reportQueryError(message, res.cause());
+      }
+    });
+  }
+
+  private void savePage(Message<JsonObject> message) {
+    JsonObject request = message.body();
+    JsonArray data = new JsonArray()
+        .add(request.getString("markdown"))
+        .add(request.getString("id"));
+
+    dbClient.updateWithParams(sqlQueries.get(SqlQuery.SAVE_PAGE), data, res -> {
+      if (res.succeeded()) {
+        message.reply("ok");
+      } else {
+        reportQueryError(message, res.cause());
+      }
+    });
+  }
+
+  private void deletePage(Message<JsonObject> message) {
+    JsonArray data = new JsonArray().add(message.body().getString("id"));
+
+    dbClient.updateWithParams(sqlQueries.get(SqlQuery.DELETE_PAGE), data, res -> {
+      if (res.succeeded()) {
+        message.reply("ok");
+      } else {
+        reportQueryError(message, res.cause());
+      }
+    });
+  }
+
+  private void reportQueryError(Message<JsonObject> message, Throwable cause) {
+    LOGGER.error("Database query error", cause);
+    message.fail(ErrorCodes.DB_ERROR.ordinal(), cause.getMessage());
+  }
+}
+
